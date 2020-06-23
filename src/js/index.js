723b3222f8bdd915fd4d969cce8683316b12e096
leszekjanczewski <leszek.janczewski@gmail.com>
2020-06-23 10:27:01 +0200

    home work

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,28 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const textarea = document.querySelector('.textarea--js')
+const save = document.querySelector('.save--js')
+const load = document.querySelector('.load--js')
+
+const currentValue = localStorage.getItem('entry');
+
+if (currentValue) {
+  document.querySelector('.info--js').innerHTML = 'ℹ️';
+}
+
+save.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('entry', textarea.value);
+  if (textarea.value) {
+    document.querySelector('.info--js').innerHTML = 'ℹ';
+  } else {
+    document.querySelector('.info--js').innerHTML = '';
+  }
+})
+
+load.addEventListener('click', (e) => {
+  e.preventDefault();
+  textarea.value = localStorage.getItem('entry');
+})
